32508ddfd6145848b363017ad631fb28947942ac
August <147424439+August-WW@users.noreply.github.com>
2024-09-29 14:27:03 -0400

    ON

---
+++ inspect start.js
@@ -0,0 +1,5 @@
+document.body.contentEditable='true'; 
+document.designMode='on'; 
+void 0
+
+// Paste into bookmark URL as: javascript:document.body.contentEditable='true'; document.designMode='on'; void 0
