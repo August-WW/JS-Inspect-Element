a32a9f3a1873c99abddbe3438570213653a1bbef
August <147424439+August-WW@users.noreply.github.com>
2024-09-29 14:27:16 -0400

    OFF

---
+++ inspect off.js
@@ -0,0 +1,5 @@
+document.body.contentEditable='false'; 
+document.designMode='off'; 
+void 100
+
+// Paste into bookmark URL as: javascript:document.body.contentEditable='false'; document.designMode='off'; void 100
